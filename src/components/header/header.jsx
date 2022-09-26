99b6e779ea528113eacf3c8ea761b0301def8f5a
SeongHo-C <leeseong010@naver.com>
2022-09-26 16:00:17 +0900

    헤더 디자인 추가

---
+++ src/components/header/header.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import styles from './header.module.css';
+
+const Header = (props) => {
+  const [isToggle, setToggle] = useState(true);
+  const [resize, setResize] = useState();
+
+  const handleResize = () => {
+    setResize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [resize]);
+
+  const toggleBtn = () => {
+    setToggle(!isToggle);
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <span className={styles.icon}>B</span>
+        <h1 className={styles.title}>바로</h1>
+      </div>
+      <ul
+        className={styles.menu}
+        style={{ display: isToggle || resize > 992 ? 'flex' : 'none' }}
+      >
+        <li>
+          <button className={styles.button}>프로젝트</button>
+        </li>
+        <li>
+          <button className={styles.button}>라운지</button>
+        </li>
+        <li>
+          <button className={styles.button}>랭킹</button>
+        </li>
+        <li>
+          <button className={styles.button}>SIGN IN</button>
+        </li>
+      </ul>
+      <button className={styles.toggle} onClick={toggleBtn}>
+        <i className='fas fa-bars'></i>
+        <span className={styles.toggleText}>menu</span>
+      </button>
+    </header>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.module.css';
 import App from './App';
+import '@fortawesome/fontawesome-free/js/all.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
