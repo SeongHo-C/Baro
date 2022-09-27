7e2d90700a1ab5553aa03a16c618c6b1c2b09f2f
SeongHo-C <leeseong010@naver.com>
2022-09-27 17:04:14 +0900

    푸터(디자인) 및 헤더(페이지 이동) 수정

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,11 @@ import styles from './footer.module.css';
 
 const Footer = (props) => {
   return (
-    <footer className={styles.footer}>
-      <i className='fa-regular fa-copyright'></i>
-      <span> Copyright 인하공업전문대학 이성호, 박인우</span>
+    <footer className={styles.container}>
+      <div className={styles.footer}>
+        <i className='fa-regular fa-copyright'></i>
+        <span> Copyright 인하공업전문대학 이성호, 박인우</span>
+      </div>
     </footer>
   );
 };
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -24,46 +24,51 @@ const Header = (props) => {
   };
 
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <span className={styles.icon}>B</span>
-        <button onClick={() => navigate('/')}>
-          <h1 className={styles.title}>바로</h1>
+    <header className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <span className={styles.icon}>B</span>
+          <button onClick={() => navigate('/')}>
+            <h1 className={styles.title}>바로</h1>
+          </button>
+        </div>
+        <ul
+          className={styles.menu}
+          style={{ display: isToggle || resize > 992 ? 'flex' : 'none' }}
+        >
+          <li>
+            <button
+              className={styles.button}
+              onClick={() => navigate('/project')}
+            >
+              프로젝트
+            </button>
+          </li>
+          <li>
+            <button
+              className={styles.button}
+              onClick={() => navigate('/rounge')}
+            >
+              라운지
+            </button>
+          </li>
+          <li>
+            <button
+              className={styles.button}
+              onClick={() => navigate('/ranking')}
+            >
+              랭킹
+            </button>
+          </li>
+          <li>
+            <button className={styles.button}>SIGN IN</button>
+          </li>
+        </ul>
+        <button className={styles.toggle} onClick={toggleBtn}>
+          <i className='fas fa-bars'></i>
+          <span className={styles.toggleText}>menu</span>
         </button>
       </div>
-      <ul
-        className={styles.menu}
-        style={{ display: isToggle || resize > 992 ? 'flex' : 'none' }}
-      >
-        <li>
-          <button
-            className={styles.button}
-            onClick={() => navigate('/project')}
-          >
-            프로젝트
-          </button>
-        </li>
-        <li>
-          <button className={styles.button} onClick={() => navigate('/rounge')}>
-            라운지
-          </button>
-        </li>
-        <li>
-          <button
-            className={styles.button}
-            onClick={() => navigate('/ranking')}
-          >
-            랭킹
-          </button>
-        </li>
-        <li>
-          <button className={styles.button}>SIGN IN</button>
-        </li>
-      </ul>
-      <button className={styles.toggle} onClick={toggleBtn}>
-        <i className='fas fa-bars'></i>
-        <span className={styles.toggleText}>menu</span>
-      </button>
     </header>
   );
 };
