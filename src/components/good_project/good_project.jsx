9c1fc23971c6807dcbb40903b9f9fa1e7baa6e44
SeongHo-C <leeseong010@naver.com>
2022-10-06 23:02:03 +0900

    프로젝트 페이지(주목할만한 프로젝트) 프로토타입 추가

--- src/components/good_project/good_project.jsx
+++ src/components/good_project/good_project.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GoodProjectCard from '../good_project_card/good_project_card';
 import styles from './good_project.module.css';
 
 const GoodProject = (props) => {
+  const [data, setData] = useState({
+    1: {
+      id: 1,
+      kind: '사이드 프로젝트',
+      image_url: '../../../images/testImage.png',
+      project_name: '아이디어 공유 및 팀빌딩 서비스',
+      content: '아이디어를 공유하고 팀을 빌딩해보세요~',
+      member: 4,
+      currentMember: 1,
+      cnt: 20,
+      recruit: '웹서버',
+    },
+    2: {
+      id: 2,
+      kind: '사이드 프로젝트',
+      image_url: '../../../images/testImage.png',
+      project_name: '아이디어 공유 및 팀빌딩 서비스',
+      content: '아이디어를 공유하고 팀을 빌딩해보세요~',
+      member: 4,
+      currentMember: 1,
+      cnt: 20,
+      recruit: '웹서버',
+    },
+    3: {
+      id: 3,
+      kind: '사이드 프로젝트',
+      image_url: '../../../images/testImage.png',
+      project_name: '아이디어 공유 및 팀빌딩 서비스',
+      content:
+        '이 프로젝트는 아이디어를 공유하는 프로젝트입니다. 대학교에 다니고있는 친구들이 프로젝트를 하는데 있어서 부담감을 줄일 수 있도록 하기 위함입니다.',
+      member: 4,
+      currentMember: 1,
+      cnt: 20,
+      recruit: '웹서버',
+    },
+  });
+
   return (
     <section className={styles.goodProject}>
-      <h1>goodProject</h1>
+      <h1>주목할만한 프로젝트</h1>
+      <ul className={styles.projects}>
+        {Object.keys(data).map((key) => (
+          <GoodProjectCard key={key} data={data[key]} />
+        ))}
+      </ul>
     </section>
   );
 };
---
+++ src/components/good_project_card/good_project_card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './good_project_card.module.css';
+
+const GoodProjectCard = ({ data }) => {
+  const { image_url, project_name, content, recruit, cnt } = data;
+
+  return (
+    <li className={styles.container}>
+      <img className={styles.img} src={image_url} alt='' />
+      <div className={styles.info}>
+        <div className={styles.top}>
+          <span>{project_name}</span>
+          <span>
+            <i className='fa-regular fa-eye'></i>
+            {` ${cnt}`}
+          </span>
+        </div>
+        <span className={styles.content}>{content}</span>
+        <span className={styles.recruit}>{`[모집중] ${recruit}`}</span>
+      </div>
+    </li>
+  );
+};
+
+export default GoodProjectCard;
