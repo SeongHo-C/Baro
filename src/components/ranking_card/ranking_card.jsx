c85af48812f779df1c7cd40d66673c1c6e774502
SeongHo-C <leeseong010@naver.com>
2022-10-30 22:06:30 +0900

    feat(랭킹): 학교 랭킹 조회 API 연결

--- src/components/ranking_card/ranking_card.jsx
+++ src/components/ranking_card/ranking_card.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './ranking_card.module.css';
 
-const RankingCard = ({ data }) => {
-  const { ranking, school, point, student } = data;
+const RankingCard = ({ data, rank }) => {
+  console.log(data);
+  const { university, point, personnel } = data;
 
   return (
     <tr>
-      <td>{ranking}</td>
-      <td>{school}</td>
+      <td>{rank}</td>
+      <td>{university}</td>
       <td>{point}</td>
-      <td>{student}</td>
+      <td>{personnel}</td>
     </tr>
   );
 };
--- src/components/ranking_team/ranking_team.jsx
+++ src/components/ranking_team/ranking_team.jsx
@@ -1,60 +1,35 @@
+import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
 import RankingCard from '../ranking_card/ranking_card';
 import styles from './ranking_team.module.css';
 
 const RankingTeam = (props) => {
-  const data = {
-    1: {
-      id: 1,
-      ranking: 1,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    2: {
-      id: 2,
-      ranking: 2,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    3: {
-      id: 3,
-      ranking: 3,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    4: {
-      id: 4,
-      ranking: 4,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    5: {
-      id: 5,
-      ranking: 5,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    6: {
-      id: 6,
-      ranking: 6,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
-    7: {
-      id: 7,
-      ranking: 7,
-      school: '인하공업전문대학',
-      point: 20000,
-      student: 2000,
-    },
+  const [rank, setRank] = useState();
+  const [page, setPage] = useState(1);
+  const url = process.env.REACT_APP_URL;
+
+  const getRankingTeam = async () => {
+    try {
+      await axios
+        .get(`${url}/rank/school?page=${page}&size=8`)
+        .then((res) => res.data)
+        .then((data) => setRank(data.content));
+    } catch (error) {
+      console.log(error);
+    }
   };
 
+  const getRank = (key) => {
+    return (page - 1) * 10 + (Number(key) + 1);
+  };
+
+  useEffect(() => {
+    getRankingTeam();
+  }, []);
+
+  console.log(rank);
   return (
     <table className={styles.table}>
       <thead className={styles.thead}>
@@ -66,9 +41,16 @@ const RankingTeam = (props) => {
         </tr>
       </thead>
       <tbody>
-        {Object.keys(data).map((key) => (
-          <RankingCard key={key} data={data[key]} />
-        ))}
+        {rank &&
+          Object.keys(rank).map((key) => {
+            return (
+              <RankingCard
+                key={key}
+                data={rank[key]}
+                rank={`${getRank(key)}`}
+              />
+            );
+          })}
       </tbody>
     </table>
   );
