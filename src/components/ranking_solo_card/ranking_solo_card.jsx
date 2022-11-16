1cc2d5185c6e1f98d9e426e0c7feafa71b1826ee
SeongHo-C <leeseong010@naver.com>
2022-11-16 18:16:07 +0900

    feat(개인 랭킹): 테이블 클릭 시 해당 사용자 프로필로 이동

--- src/components/ranking_solo_card/ranking_solo_card.jsx
+++ src/components/ranking_solo_card/ranking_solo_card.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './ranking_solo_card.module.css';
 
 const RankingSoloCard = ({ data, rank }) => {
-  const { university, nickname, point } = data;
+  const { id, university, nickname, point } = data;
+
+  const navigate = useNavigate();
+  const moveProfile = () => {
+    navigate(`/profile/${id}`);
+  };
 
   return (
-    <tr className={styles.card}>
+    <tr className={styles.card} onClick={moveProfile}>
       <td>{rank}</td>
       <td>{university}</td>
       <td>{nickname}</td>
